abcf7a17c6715d636a780622eafaf9c97d2874d2
Jakeukalane <31076722+Jakeukalane@users.noreply.github.com>
2018-10-07 18:05:58 +0200

    Update listado-urls.js

--- listado-urls.js
+++ listado-urls.js
@@ -1,5 +1,6 @@
 (function (window){
     var urls = [
+        'http://tuscriaturas.blogia.com/2018/092901-los-buscadores.php',
         'http://tuscriaturas.blogia.com/2018/092501-los-owonumo.php',
         'http://tuscriaturas.blogia.com/2018/091701-el-planeta-gemraid-i-la-caida-de-oth-rendor.php',
         'http://tuscriaturas.blogia.com/2018/091001-las-ventanas-de-contenido.php',
