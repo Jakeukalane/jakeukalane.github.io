9c532f5b797530f715e9b5cad1a4c62df671a3f8
Jakeukalane <31076722+Jakeukalane@users.noreply.github.com>
2018-11-20 13:56:14 +0100

    Update listado-urls.js

--- listado-urls.js
+++ listado-urls.js
@@ -1,5 +1,6 @@
 (function (window){
     var urls = [
+        'http://tuscriaturas.blogia.com/2018/112001-los-terrorgrifos.php',
         'http://tuscriaturas.blogia.com/2018/111201-como-invitar-al-yeti-a-un-cacao.php',
         'http://tuscriaturas.blogia.com/2018/110501-el-planeta-o-shyhon-iii-la-primera-gran-guerra.php',
         'http://tuscriaturas.blogia.com/2018/102901-los-senores-de-la-consciencia.php',
