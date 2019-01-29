89eddd2072272e984a3617a4efb717ce0e7bfb32
Jakeukalane <31076722+Jakeukalane@users.noreply.github.com>
2019-01-29 17:58:55 +0100

    Update listado-urls.js

--- listado-urls.js
+++ listado-urls.js
@@ -1,5 +1,6 @@
 (function (window){
     var urls = [
+        'http://tuscriaturas.blogia.com/2019/012801-las-proyecciones-de-aspecto.php',
         'https://tuscriaturas.blogia.com/2019/012201-las-quimeras-metamorficas.php',
         'https://tuscriaturas.blogia.com/2019/011401-la-ley-de-las-probabilidades-acumuladas.php',
         'https://tuscriaturas.blogia.com/2019/010701-aventuras-en-egipto.php',
